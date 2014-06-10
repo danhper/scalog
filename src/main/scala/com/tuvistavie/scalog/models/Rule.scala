c1335d46f8711b3bf236783727d907d23f6df40d
Daniel Perez <tuvistavie@gmail.com>
2014-06-10 15:07:29 +0900

    Remove unneeded equal and hashCode methods.

--- src/main/scala/com/tuvistavie/scalog/models/Rule.scala
+++ src/main/scala/com/tuvistavie/scalog/models/Rule.scala
@@ -8,18 +8,8 @@ sealed trait Rule {
 
 case class Fact(head: Atom) extends Rule {
   override def toString: String = head.toString
-  override def equals(o: Any) = o match {
-    case that: Clause => head == that.head
-    case _ => false
-  }
-  override def hashCode = head.hashCode
 }
 
 case class Clause(head: Atom, body: Formula) extends Rule {
   override def toString: String = head + " :- " + body
-  override def equals(o: Any) = o match {
-    case that: Clause => head == that.head
-    case _ => false
-  }
-  override def hashCode = head.hashCode
 }
